c6b3eb3c9e2fa2ef6f0957400e9b6a2d6453b0a2
Alifnewjourney Jovani safik <furqonseli@gmail.com>
2025-06-18 20:35:04 +0700

    A predefined discount

--- src/app/discount/page.tsx
+++ src/app/discount/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import ReportMain from "../../components/reports/ReportMain";
+
 
 export default function Reports() {
     return (
         <div className="min-h-screen text-black flex flex-col bg-gray-100">
-            <ReportMain />
+            <h1>Discount</h1>
         </div>
     )
 }
